d1f968bf9f69d0853ae8aa53548cda97e9993b89
Christopher H. Casebeer <christopher@chc.name>
2026-03-03 01:49:26 +0000

    Test HDHR TCP control protocol client

---
+++ client.py
@@ -0,0 +1,33 @@
+
+import socket
+import hdhr
+
+import os
+
+HOST = os.environ.get('HOST')
+PORT = int(os.environ.get('PORT', 65001))
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.connect((HOST, PORT))
+
+    payload = hdhr.Payload(
+        fields=[hdhr.PayloadField(
+            tag=hdhr.PayloadTag.HDHOMERUN_TAG_GETSET_NAME,
+            value=bytearray("/sys/version\0", encoding="ascii"),
+        )]
+    )
+
+    packet = hdhr.Packet(
+        packetType=hdhr.PacketType.HDHOMERUN_TYPE_GETSET_REQ,
+        payload=payload,
+    )
+    print("Sending")
+    print(packet)
+
+    sock.sendall(packet.unparse())
+    data = sock.recv(2048)
+    print("Received")
+    print(data.hex())
+
+    response = hdhr.Packet.parse(data)
+    print(response)
