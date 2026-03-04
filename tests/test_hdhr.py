e7190b7c10974c4c5932ce06ab53e01b4ea35cc4
Christopher H. Casebeer <christopher@chc.name>
2026-03-04 05:31:13 +0000

    Add (generated) Discovery Protocol packet test

--- tests/test_hdhr.py
+++ tests/test_hdhr.py
@@ -1,6 +1,9 @@
 
+import logging
 import unittest
 import hdhr
+
+logger = logging.getLogger(__name__)
 
 class TestPayloadField(unittest.TestCase):
     lengthTestVectors = [
@@ -18,8 +21,17 @@ class TestPayloadField(unittest.TestCase):
             self.assertEqual(length, expectedLength)
 
 class TestPacket(unittest.TestCase):
-    errorResponse = bytes.fromhex("0005002205204552524f523a206d616c666f726d65642067657473657420726571756573740037494141")
-    versionResponse = bytes.fromhex("0005001a030d2f7379732f76657273696f6e000409323032353036323300053b1257")
+    errorResponse = bytes.fromhex(
+        "0005002205204552524f523a206d616c666f726d65642067657473657420"
+        "726571756573740037494141") # captured from HDTC-2US
+    versionResponse = bytes.fromhex(
+        "0005001a030d2f7379732f76657273696f6e000409323032353036323300"
+        "053b1257") # captured from HDTC-2US
+    discoverResponse = bytes.fromhex(
+        "000300690104000000012d04000000010204010203042b18313233343536"
+        "3738393031323334353637383930313233342a15687474703a2f2f313932"
+        "2e302e322e3132333a38301001022721687474703a2f2f3139322e302e32"
+        "2e3132333a38302f6c696e6575702e6a736f6e67e72021") # generated
 
     def test_parseVersionResponse(self):
         packet = hdhr.Packet.parse(self.versionResponse)
@@ -38,6 +50,36 @@ class TestPacket(unittest.TestCase):
         self.assertEqual(packet.packetType, hdhr.PacketType.GETSET_RPY)
         self.assertEqual(packet.payload.fields[0].tag, hdhr.PayloadTag.ERROR_MESSAGE)
 
+    def test_parseDiscoveryResponse(self):
+        logger.debug("Parsing discovery response packet...")
+        packet = hdhr.Packet.parse(self.discoverResponse)
+
+        logger.debug(packet)
+
+        self.assertEqual(packet.packetType, hdhr.PacketType.DISCOVER_RPY)
+
+        self.assertEqual(packet.payload.fields[0].tag, hdhr.PayloadTag.DEVICE_TYPE)
+        self.assertEqual(packet.payload.fields[0].value, b"\00\00\00\01")
+
+        self.assertEqual(packet.payload.fields[1].tag, hdhr.PayloadTag.MULTI_TYPE)
+        self.assertEqual(packet.payload.fields[1].value, b"\00\00\00\01")
+
+        self.assertEqual(packet.payload.fields[2].tag, hdhr.PayloadTag.DEVICE_ID)
+        self.assertEqual(packet.payload.fields[2].value, b"\01\02\03\04")
+
+        self.assertEqual(packet.payload.fields[3].tag, hdhr.PayloadTag.DEVICE_AUTH_STR)
+        self.assertEqual(packet.payload.fields[3].value, b"123456789012345678901234")
+
+        self.assertEqual(packet.payload.fields[4].tag, hdhr.PayloadTag.BASE_URL)
+        self.assertEqual(packet.payload.fields[4].value, b"http://192.0.2.123:80")
+
+        self.assertEqual(packet.payload.fields[5].tag, hdhr.PayloadTag.TUNER_COUNT)
+        self.assertEqual(packet.payload.fields[5].value, b"\02")
+
+        self.assertEqual(packet.payload.fields[6].tag, hdhr.PayloadTag.LINEUP_URL)
+        self.assertEqual(packet.payload.fields[6].value,
+            b"http://192.0.2.123:80/lineup.json")
+
     def test_unparseVersionResponse(self):
         packet = hdhr.Packet.parse(self.versionResponse)
         self.assertEqual(packet.unparse(), self.versionResponse)
@@ -46,6 +88,11 @@ class TestPacket(unittest.TestCase):
         packet = hdhr.Packet.parse(self.errorResponse)
         self.assertEqual(packet.unparse(), self.errorResponse)
 
+    def test_unparseDiscoveryResponse(self):
+        packet = hdhr.Packet.parse(self.discoverResponse)
+        self.assertEqual(packet.unparse(), self.discoverResponse)
+
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
     unittest.main()
